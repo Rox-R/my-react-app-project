94e7092bf6268dd352e0838df2d787d607f65d4a
Rox-R <68868124+Rox-R@users.noreply.github.com>
2021-01-09 17:36:09 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App"> 
+    <div className="container">
     <h1> Weather app </h1> 
     <footer> 
       This project was coded by Roxana Rognean and is 
     <a href="https://github.com/Rox-R/my-react-app-project" target="_blank" rel="noreferrer"> open-sourced on GitHub</a> 
 </footer>
+</div>
     </div>);
       
         

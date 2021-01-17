8bb906d02565c77590a1f8bac67bed0ed85d7be0
Rox-R <68868124+Rox-R@users.noreply.github.com>
2021-01-17 14:11:37 +0100

    added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"; 
 import './App.css';
+import Weather from "./Weather"; 
 
 export default function App() {
   return (
     <div className="App"> 
     <div className="container">
     <h1> Weather app </h1> 
-    <footer> 
+    <Weather /> 
+    <footer className="footer"> 
       This project was coded by Roxana Rognean and is 
     <a href="https://github.com/Rox-R/my-react-app-project" target="_blank" rel="noreferrer"> open-sourced on GitHub</a> 
 </footer>
---
+++ src/Weather.js
@@ -0,0 +1,43 @@
+import React from "react"; 
+import "./Weather.css"; 
+
+export default function Weather() {
+    return (
+        
+        <div className="Weather"> 
+        <form> 
+            <div className="row"> 
+            <div className="col-5"> 
+            <input type="search" placeholder="Enter city" className="form-control"
+            autoFocus="on" />
+        </div>
+        <div className="col-3"> 
+        <input type="submit" value="search" className="btn btn-primary w-100" /> 
+        </div>
+        </div>
+        </form>
+        <h1> Amsterdam </h1>
+        <ul>
+            <li> Sunday 16:00 </li>
+            <li> Mostly Sunny </li>
+        </ul>
+        <div className="row"> 
+        <div className="col-7">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+            alt="Mostly sunny" className="weather-icon"
+            />
+            <span className="temperature">5</span> 
+            <span className="units">°C</span>
+        </div>
+        <div className="col-6">
+            <ul>
+                <li>Precipitation: 6% </li>
+                <li> Humidity: 83% </li>
+                <li> Wind: 16km/h </li>
+            </ul>
+        </div>
+        </div>
+        
+        </div>
+    );
+}
